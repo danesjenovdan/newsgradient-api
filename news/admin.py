a206f348f35982fe357df852d41d49e092bd71a1
Tomaž <cofek0@gmail.com>
2019-08-14 15:27:32 +0200

    added editor for medium slant

--- news/admin.py
+++ news/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from news import models
 # Register your models here.
 
-admin.site.register(models.Medium)
+class MediumAdmin(admin.ModelAdmin):
+    list_display = ['title', 'uri', 'slant']
+    search_fields = ['title', 'uri']
+    list_filter = ['slant']
+    list_editable = ('slant',)
+
+admin.site.register(models.Medium, MediumAdmin)
 admin.site.register(models.Event)
 admin.site.register(models.Article)
