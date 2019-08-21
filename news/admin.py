c42f8225c73292ecc164ccb93612e4586cb34c1c
Tomaž <cofek0@gmail.com>
2019-08-21 15:47:34 +0200

    added admin for edit events visibility

--- news/admin.py
+++ news/admin.py
@@ -8,6 +8,16 @@ class MediumAdmin(admin.ModelAdmin):
     list_filter = ['slant']
     list_editable = ('slant',)
 
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['title', 'date', 'get_articles_count', 'is_hidden']
+    search_fields = ['title', 'summary']
+    list_filter = ['date', 'is_hidden']
+    list_editable = ('is_hidden',)
+
+    def get_articles_count(self, obj):
+        return obj.articles.count()
+
 admin.site.register(models.Medium, MediumAdmin)
-admin.site.register(models.Event)
+admin.site.register(models.Event, EventAdmin)
 admin.site.register(models.Article)
