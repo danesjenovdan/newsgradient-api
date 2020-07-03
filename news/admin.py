4575bb8de9c3eb6345ed1ac97e10254ebe9ed93f
Klemen Strajhar <klemen.strajhar@gmail.com>
2020-07-03 11:15:01 +0200

    Add display for number of articles

--- news/admin.py
+++ news/admin.py
@@ -4,30 +4,55 @@ from django.db.models import IntegerField
 from django.db.models import Q
 from django.db.models.functions import Cast
 
+from constants import Orientations
 from news import models
 
 
 # Register your models here.
 
 class MediumAdmin(admin.ModelAdmin):
-    list_display = ['title', 'uri', 'slant']
+    list_display = ['title', 'uri', 'slant', 'favicon']
     search_fields = ['title', 'uri']
     list_filter = ['slant']
     list_editable = ('slant',)
 
 
 class EventAdmin(admin.ModelAdmin):
-    list_display = ['title', 'date', 'number_of_articles', 'article_count', 'is_promoted']
+    list_display = ['title', 'date', 'number_of_articles', 'is_promoted', 'left_count', 'neutral_count', 'right_count']
     search_fields = ['title', 'summary']
     list_filter = ['date', 'is_promoted']
     list_editable = ('is_promoted',)
 
     def get_queryset(self, request):
-        return models.Event.objects.all().annotate(
-            all_count=Cast(
+        return models.Event.objects.prefetch_related('articles').all().annotate(
+            _all_count=Cast(
                 Count(
                     'articles',
                     filter=Q(articles__medium__slant__isnull=False),
+                    distinct=True
+                ),
+                IntegerField()
+            ),
+            _left_count=Cast(
+                Count(
+                    'articles',
+                    filter=Q(articles__medium__slant=Orientations.LEFT.value),
+                    distinct=True
+                ),
+                IntegerField()
+            ),
+            _neutral_count=Cast(
+                Count(
+                    'articles',
+                    filter=Q(articles__medium__slant=Orientations.NEUTRAL.value),
+                    distinct=True
+                ),
+                IntegerField()
+            ),
+            _right_count=Cast(
+                Count(
+                    'articles',
+                    filter=Q(articles__medium__slant=Orientations.RIGHT.value),
                     distinct=True
                 ),
                 IntegerField()
@@ -35,7 +60,21 @@ class EventAdmin(admin.ModelAdmin):
         )
 
     def number_of_articles(self, obj):
-        return obj.all_count
+        return obj._all_count
+
+    def left_count(self, obj):
+        return obj._left_count
+
+    def neutral_count(self, obj):
+        return obj._neutral_count
+
+    def right_count(self, obj):
+        return obj._right_count
+
+    number_of_articles.admin_order_field = '_all_count'
+    left_count.admin_order_field = '_left_count'
+    neutral_count.admin_order_field = '_neutral_count'
+    right_count.admin_order_field = '_right_count'
 
 
 class ArticleAdmin(admin.ModelAdmin):
