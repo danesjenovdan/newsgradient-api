0e4b72878d271fc5706502fc2cc08619c0781085
LepkoQQ <lepko.san@gmail.com>
2020-07-14 20:59:36 +0200

    serve static files during development

--- newsgradient/urls.py
+++ newsgradient/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('backoffice/', include('backoffice.urls')),
     path('api/v1/', include('api_versioning.urls_v1')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
