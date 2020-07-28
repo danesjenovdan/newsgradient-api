2642a811944ef572d1629312c447f7f9bfaf8a84
Tomaz <tomazkunst@gmail.com>
2020-07-28 18:30:18 +0200

    added actions to admin/event for merging events

--- news/admin.py
+++ news/admin.py
@@ -1,12 +1,13 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from django.db.models import Count
 from django.db.models import IntegerField
 from django.db.models import Q
 from django.db.models.functions import Cast
+from django.template.response import TemplateResponse
+from django.utils.translation import ngettext
 
 from constants import Orientations
 from news import models
-
 
 # Register your models here.
 
@@ -70,6 +71,49 @@ class EventAdmin(admin.ModelAdmin):
 
     def right_count(self, obj):
         return obj._right_count
+
+    def merge_to_oldest(self, request, queryset):
+        first_event = queryset.earliest('date')
+        other_events = queryset.exclude(uri=first_event.uri)
+
+        if request.POST.get('post'):
+            for event in other_events:
+                event.articles.all().update(event=first_event)
+            other_events.delete()
+            self.message_user(request, 'Articles was successfully added to event: %s' %first_event.title , messages.SUCCESS)
+        else:
+            request.current_app = self.admin_site.name
+            lala = {
+                'queryset': queryset,
+                'action': 'merge_to_oldest',
+                'parent_event': first_event,
+                'other_events': other_events,
+                'c_action': 'move articles of events to oldest event and delete newest events.',
+            }
+            return TemplateResponse(request, "admin/action_confirmation.html", context=lala)
+
+    def merge_to_most_popular(self, request, queryset):
+        print(queryset)
+        sorted_events = queryset.annotate(ac=Count('articles')).order_by('-ac')
+        first_event = sorted_events[0]
+        other_events = sorted_events.exclude(uri=first_event.uri)
+        if request.POST.get('post'):
+            for event in other_events:
+                event.articles.all().update(event=first_event)
+            other_events.delete()
+            self.message_user(request, 'Articles was successfully added to event: %s' %first_event.title , messages.SUCCESS)
+        else:
+            request.current_app = self.admin_site.name
+            lala = {
+                'queryset': queryset,
+                'parent_event': first_event,
+                'action': 'merge_to_most_popular',
+                'other_events': other_events,
+                'c_action': 'move articles of events to the most popular event and delete other events.'
+            }
+            return TemplateResponse(request, "admin/action_confirmation.html", context=lala)
+
+    actions = [merge_to_oldest, merge_to_most_popular]
 
     number_of_articles.admin_order_field = '_all_count'
     left_count.admin_order_field = '_left_count'
